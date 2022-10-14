f59af9510c4bb54a8d780b8aac5a65b420804880
John Tarleton Moore <tarletonmoore@gmail.com>
2022-10-14 13:32:35 -0400

    added just series

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  
   get "/series" do
+    series = Series.all
+    series.to_json
+  end
+
+
+  
+  get "/series/games" do
     series = Series.all
     series.to_json(include: :games)
   end
