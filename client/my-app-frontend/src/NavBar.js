e285fcfc309e7a23a3c992a2e9c575a494e5c862
John Tarleton Moore <tarletonmoore@gmail.com>
2022-10-28 13:35:20 -0400

    navbar changes

--- client/my-app-frontend/src/NavBar.js
+++ client/my-app-frontend/src/NavBar.js
@@ -2,23 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom"
 
 function NavBar() {
-
+    const linkStyles = {
+        width: "100px",
+        padding: "12px",
+        margin: "0 6px 6px",
+        color: "white",
+    };
     return (
         <div>
-            {/* <NavLink
-                to="/"
 
-            >
-                Home
-            </NavLink> */}
             <NavLink
                 to="/games"
+                style={linkStyles}
 
             >
                 Games
             </NavLink>
             <NavLink
                 to="/add"
+                style={linkStyles}
             >
                 Add Game
             </NavLink>
