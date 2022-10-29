c322525694784b746c5c0c37c252ce7e94b8ec84
John Tarleton Moore <tarletonmoore@gmail.com>
2022-10-29 13:22:55 -0400

    app con cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,10 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  
   get "/series" do
     series = Series.all
     series.to_json(include: :games)
   end
-
-#   get "/series/:id" do
-# series = Series.find(params[:id])
-# series.to_json(include: :games)
-
-#   end
-
-# get "/games/:id" do
-#   game = Game.find(params[:id])
-#   game.to_json
-# end
-  
-  # get "/series/games" do
-  #   series = Series.all
-  #   series.to_json(include: :games)
-  # end
 
 get "/games" do
   games = Game.all
@@ -37,9 +19,6 @@ game = Game.create(
   console: params[:console],
   series_id: params[:series_id]
 )
-# game.series.create(
-#   title: params[:series]
-# )
 game.to_json(include: :series)
 end
 
